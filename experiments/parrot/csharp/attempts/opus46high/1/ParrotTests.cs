6a79eaafb781147151be1f955ccd5fd1438735da
Jo Van Eyck <jovaneyck1988@hotmail.com>
2026-04-30 11:35:06 +0200

    parrot c# 1

--- experiments/parrot/csharp/attempts/opus46high/1/ParrotTests.cs
+++ experiments/parrot/csharp/attempts/opus46high/1/ParrotTests.cs
@@ -7,84 +7,98 @@ public class ParrotTests
     [Fact]
     public void SpeedOfEuropeanParrot()
     {
-        var parrot = new Parrot(ParrotTypeEnum.EUROPEAN, 0, 0, false);
+        var parrot = Parrot.Create(ParrotTypeEnum.EUROPEAN, 0, 0, false);
         Assert.Equal(12.0, parrot.GetSpeed());
     }
 
     [Fact]
     public void SpeedOfAfricanParrotWithOneCoconut()
     {
-        var parrot = new Parrot(ParrotTypeEnum.AFRICAN, 1, 0, false);
+        var parrot = Parrot.Create(ParrotTypeEnum.AFRICAN, 1, 0, false);
         Assert.Equal(3.0, parrot.GetSpeed());
     }
 
     [Fact]
     public void SpeedOfAfricanParrotWithTwoCoconuts()
     {
-        var parrot = new Parrot(ParrotTypeEnum.AFRICAN, 2, 0, false);
+        var parrot = Parrot.Create(ParrotTypeEnum.AFRICAN, 2, 0, false);
         Assert.Equal(0.0, parrot.GetSpeed());
     }
 
     [Fact]
     public void SpeedOfAfricanParrotWithNoCoconuts()
     {
-        var parrot = new Parrot(ParrotTypeEnum.AFRICAN, 0, 0, false);
+        var parrot = Parrot.Create(ParrotTypeEnum.AFRICAN, 0, 0, false);
         Assert.Equal(12.0, parrot.GetSpeed());
     }
 
     [Fact]
     public void SpeedOfNorwegianBlueParrotNailed()
     {
-        var parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 0, true);
+        var parrot = Parrot.Create(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 0, true);
         Assert.Equal(0.0, parrot.GetSpeed());
     }
 
     [Fact]
     public void SpeedOfNorwegianBlueParrotNailedWithVoltage()
     {
-        var parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 1.5, true);
+        var parrot = Parrot.Create(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 1.5, true);
         Assert.Equal(0.0, parrot.GetSpeed());
     }
 
     [Fact]
     public void SpeedOfNorwegianBlueParrotNotNailed()
     {
-        var parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 1.5, false);
+        var parrot = Parrot.Create(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 1.5, false);
         Assert.Equal(18.0, parrot.GetSpeed());
     }
 
     [Fact]
     public void SpeedOfNorwegianBlueParrotNotNailedHighVoltage()
     {
-        var parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 4, false);
+        var parrot = Parrot.Create(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 4, false);
         Assert.Equal(24.0, parrot.GetSpeed());
     }
 
     [Fact]
     public void CryOfEuropeanParrot()
     {
-        var parrot = new Parrot(ParrotTypeEnum.EUROPEAN, 0, 0, false);
+        var parrot = Parrot.Create(ParrotTypeEnum.EUROPEAN, 0, 0, false);
         Assert.Equal("Sqoork!", parrot.GetCry());
     }
 
     [Fact]
     public void CryOfAfricanParrot()
     {
-        var parrot = new Parrot(ParrotTypeEnum.AFRICAN, 2, 0, false);
+        var parrot = Parrot.Create(ParrotTypeEnum.AFRICAN, 2, 0, false);
         Assert.Equal("Sqaark!", parrot.GetCry());
     }
 
     [Fact]
     public void CryOfNorwegianBlueParrotHighVoltage()
     {
-        var parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 4, false);
+        var parrot = Parrot.Create(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 4, false);
         Assert.Equal("Bzzzzzz", parrot.GetCry());
     }
 
     [Fact]
     public void CryOfNorwegianBlueParrotNoVoltage()
     {
-        var parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 0, false);
+        var parrot = Parrot.Create(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 0, false);
         Assert.Equal("...", parrot.GetCry());
+    }
+
+    [Fact]
+    public void SpeedOfExoticParrot()
+    {
+        var parrot = Parrot.Create(ParrotTypeEnum.EXOTIC);
+        Assert.Equal(4.0, parrot.GetSpeed());
+    }
+
+    [Fact]
+    public void CryOfExoticParrot()
+    {
+        var parrot = Parrot.Create(ParrotTypeEnum.EXOTIC);
+        Assert.Equal("squaaaawk-exotic", parrot.GetCry());
     }
 }
